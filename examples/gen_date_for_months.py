bfe41cd23c617625331186bb2d7ab3bd3c4b0162
Aadvik <aadv1k@outlook.com>
2022-03-26 22:48:38 +0530

    :bug: :hammer: bug fix in `examples/gen_date_for_months.py`

--- examples/gen_date_for_months.py
+++ examples/gen_date_for_months.py
@@ -1,6 +1,7 @@
 import os
+import json
 
-def gen_months(months, year):
+def gen_months(months, year, timetable):
     for mdate,mname in enumerate(months, start=1):
         m = f'{mdate}_{mname}_{year}'
         os.mkdir(m)
@@ -8,16 +9,16 @@ def gen_months(months, year):
 
         for i in range(4):
             with open(f'{m}/{i}.json', 'w') as file:
-                file.write(json.dumps(so.get_timetable(f"2021-{mdate}-{d}", f"2021-{mdate}-{d+6}")))
+                file.write(json.dumps(timetable(f"2021-{mdate}-{d}", f"2021-{mdate}-{d+6}")))
             d+=1+6
 
 
-def gen_month(mname, mdate, year):
-    m= f'{mname}_{mdate}_{year}'
+def gen_month(mname, mdate, year, timetable):
+    m=f'{mname}_{mdate}_{year}'
     os.mkdir(m)
 
     for i in range(4):
         d = 1
         with open(f'{m}/{i}.json', 'w') as file:
-            file.write(json.dumps(so.get_timetable(f"{year}-{mdate}-{d}", f"{year}-{mdate}-{d+6}")))
+            file.write(json.dumps(timetable(f"{year}-{mdate}-{d}", f"{year}-{mdate}-{d+6}")))
         d+=1+6
